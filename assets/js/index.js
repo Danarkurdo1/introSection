feffbeae706853e522b95b963c21d7746e661831
danar kurdo <37774241+Danarkurdo1@users.noreply.github.com>
2022-05-11 13:42:11 +0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+var w = window.innerWidth;
+if (w <= 400){
+    document.getElementById('imghero').src="assets/image/image-hero-mobile.png";
+}else{
+    document.getElementById('imghero').src="assets/image/image-hero-desktop.png";
+}
